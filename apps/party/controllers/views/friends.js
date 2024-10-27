a2fe878bdb2958a4e81d5b4cd4102add6c7b181e
Colby Roberts <colbiverse@gmail.com>
2024-10-27 15:40:49 -0700

    added friends and friend request backend

--- apps/party/controllers/views/friends.js
+++ apps/party/controllers/views/friends.js
@@ -4,9 +4,14 @@ class FriendsController {
     static async render(req, res) {
         const user = req.session.user;
 
+        const friends = FriendsService.getFriendsByUserId(user.id);
+        const requests = FriendsService.getFriendRequestsByUserId(user.id);
+
         res.render('friends/friends', {
             section: 'friends',
-            user
+            user,
+            friends,
+            requests
         });
     }
 }
--- apps/party/services/friends.js
+++ apps/party/services/friends.js
@@ -1,7 +1,148 @@
 const db = require('../../../utils/database');
 
 class FriendsService {
-    
+    static async getFriendsByUserId(userId) {
+        let result = await db.execute(`
+            SELECT 
+                userOne.id,
+                userOne.username,
+                userTwo.id,
+                userTwo.username,
+                friend.status
+                
+            FROM 
+                friend 
+
+                INNER JOIN user userOne ON
+                    userOne.id = friend.userOneId
+
+                INNER JOIN user userTwo ON
+                    userTwo.id = friend.userTwoId
+            
+            WHERE 
+                (
+                    friend.userOneId = [userId] OR
+                    friend.userTwoId = [userId]
+                ) AND
+                friend.status = 'accepted';`,
+            {
+                userId
+            }
+        );
+
+        if (!result.rows.length) {
+            return [];
+        }
+
+        return result.rows.map(row => {
+            if (row.userTwo.id != userId) {
+                row.userTwo.status = row.friend.status;
+                return {
+                    userId: row.userTwo.id,
+                    username: row.userTwo.username
+                };
+            } else {
+                row.userOne.status = row.friend.status;
+                return {
+                    userId: row.userOne.id,
+                    username: row.userOne.username
+                };
+            }
+        });
+    }
+
+    static async getFriendRequestsByUserId(userId) {
+        let result = await db.execute(`
+            SELECT
+                userOne.id,
+                userOne.username,
+                userTwo.id,
+                userTwo.username,
+                friend.status
+                
+            FROM 
+                friend 
+
+                INNER JOIN user userOne ON
+                    userOne.id = friend.userOneId
+
+                INNER JOIN user userTwo ON
+                    userTwo.id = friend.userTwoId
+            
+            WHERE 
+                (
+                    friend.userOneId = [userId] OR
+                    friend.userTwoId = [userId]
+                ) AND
+                friend.status = 'pending';`,
+                
+            {
+                userId
+            }
+        );
+
+        if (!result.rows.length) {
+            return [];
+        }
+
+        let requests = [];
+
+        for (let row of result.rows) {
+            let request = {};
+
+            if (row.userTwo.id != userId) {
+                request = {
+                    userId: row.userTwo.id,
+                    username: row.userTwo.username
+                };
+            } else {
+                request = {
+                    userId: row.userOne.id,
+                    username: row.userOne.username
+                };
+            }
+
+            request.status = row.friend.status;
+
+            requests.push(request)
+        }
+
+        return requests;
+    }
+
+    static async addFriend(userOneId, userTwoId) {
+        let result =  await db.insert('friend',  { userOneId, userTwoId }); // data = {userOneId, userTwoId}
+
+        return !result.error;
+    }
+
+    static async updateStatus(userOneId, userTwoId, status) { // works even if user id's are backwards (probably idk i didnt test it :P )
+        let result = await db.execute(`
+            UPDATE 
+                friend
+
+            SET 
+                status = [status]
+
+            WHERE
+                (
+                    userOneId = [userOneId] AND
+                    userTwoId = [userTwoId]
+                ) 
+                OR
+                (
+                    userOneId = [userTwoId] AND
+                    userTwoId = [userOneId]
+                );`,
+            {
+                userOneId, 
+                userTwoId, 
+                status
+            }
+        );
+
+        return !result.error;
+    }
 }
 
 module.exports = FriendsService;
