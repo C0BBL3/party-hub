88ef52c46917fc293b5168b36175a826d682dd46
Colby Roberts <colbiverse@gmail.com>
2024-10-27 15:49:05 -0700

    added tips for friends screen

--- apps/party/controllers/views/friends.js
+++ apps/party/controllers/views/friends.js
@@ -4,8 +4,8 @@ class FriendsController {
     static async render(req, res) {
         const user = req.session.user;
 
-        const friends = FriendsService.getFriendsByUserId(user.id);
-        const requests = FriendsService.getFriendRequestsByUserId(user.id);
+        const friends = await FriendsService.getFriendsByUserId(user.id);
+        const requests = await FriendsService.getFriendRequestsByUserId(user.id);
 
         res.render('friends/friends', {
             section: 'friends',
