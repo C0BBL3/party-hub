91d1e53a982cf788149f47fa2a5781fa70f81089
Colby Roberts <colbiverse@gmail.com>
2024-11-22 18:31:59 -0800

    added ability to RSVP for parties

--- apps/party/controllers/api/rsvp.js
+++ apps/party/controllers/api/rsvp.js
@@ -1,7 +1,32 @@
 const RSVPService = require('../../services/rsvp');
 
 class RSVPAPIController {
-   
+    static async rsvp(req, res) {
+        const user = req.session.user;
+        const { partyId, userId } = req.body;
+
+        if (user.id != userId) {
+            return res.send({ result: false });
+        }
+
+        const secretKey = RSVPAPIController.generateSecretKey();
+
+        const rsvpId = await RSVPService.rsvp(partyId, userId, secretKey);
+
+        return res.send({ result: true });
+    }
+
+    static generateSecretKey(length = 32) {
+        const chars = 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // base 36 w/ capital letters
+        
+        let secretKey = '';
+
+        for (let i = 0; i < length; i++) {
+            secretKey += chars[Math.floor(Math.random() * chars.length)];
+        }
+
+        return secretKey;
+    }
 }
 
 module.exports = RSVPAPIController;
--- apps/party/public/feed/feed-api.js
+++ apps/party/public/feed/feed-api.js
@@ -1,5 +1,8 @@
 api.feed = {
     getFirst10Parties: async () => {
         return await api.get(`${API_BASE_URL}/party/feed/get-first-10-parties`);
+    },
+    rsvp: async (partyId, userId) => {
+        return await api.post(`${API_BASE_URL}/party/rsvp`, { partyId, userId });
     }
 }
